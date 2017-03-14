3c43c08530799f20053414d1a48ab955b616b642
PCEduardo <damianlema@gmail.com>
2017-03-14 10:54:25 -0430

    Prueba

--- conf/conex.php
+++ conf/conex.php
@@ -19,7 +19,7 @@ function Conectarse()
         exit();
     }
 
-    if (!mysql_select_db("gestion_barrancas_2016", $link)) {
+    if (!mysql_select_db("gestion_mariguitar_2017", $link)) {
 
         echo "Error conectando a la base de datos.";
         exit();
