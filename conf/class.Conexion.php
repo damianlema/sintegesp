11176c55153eb52cf0a6a85d68f1de7d7f0ea96e
Dell nuevo <damianlema@gmail.com>
2017-07-02 18:19:09 -0400

    prueba de paquetes sublimetext

--- conf/class.Conexion.php
+++ conf/class.Conexion.php
@@ -1,29 +1,43 @@
 <?php
+/*******************************************************************************************************************
+ * @Clase para conectarse a la base de datos y metodos basicos
+ * @version: 1.0
+ * @Fecha creacion:
+ * @Autor: Hector Lema
+ ********************************************************************************************************************
+ * @Fecha Modificacion:
+ * @Autor:
+ * @Descripcion:
+ ********************************************************************************************************************/
 
-class Conexion extends mysqli {
+class Conexion extends mysqli
+{
 
-	private static $db_host = 'localhost';
-	private static $db_user = 'root';
-	private static $db_pass = '';
-	private static $db_name = 'gestion_gobernacion_2017_08052017';
+    private static $db_host = 'localhost';
+    private static $db_user = 'root';
+    private static $db_pass = '';
+    private static $db_name = 'gestion_gobernacion_2017_08052017';
 
-	public function __construct(){
-		parent::__construct(self::$db_host,self::$db_user,self::$db_pass,self::$db_name);
-		$this->query("SET NAMES utf8;");
-		$this->connect_errno ? die('ERROR: conexion a BD fallida') : null;
-	}
+    public function __construct()
+    {
+        parent::__construct(self::$db_host, self::$db_user, self::$db_pass, self::$db_name);
+        $this->query("SET NAMES utf8;");
+        $this->connect_errno ? die('ERROR: conexion a BD fallida') : null;
+    }
 
-	public function rows($x){
-		return mysqli_num_rows($x);
-	}
+    public function rows($x)
+    {
+        return mysqli_num_rows($x);
+    }
 
-	public function recorrer($x){
-		return mysqli_fetch_array($x);
-	}
+    public function recorrer($x)
+    {
+        return mysqli_fetch_array($x);
+    }
 
-	public function liberar($x){
-		return mysqli_free_result($x);
-	}
+    public function liberar($x)
+    {
+        return mysqli_free_result($x);
+    }
 
 }
-?>
--- funciones/validar_minutos_transcurridos_ajax.php
+++ funciones/validar_minutos_transcurridos_ajax.php
@@ -1,22 +1,21 @@
 <?
 session_start();
-include("../conf/conex.php");
-include("../funciones/funciones.php");
+include "../conf/conex.php";
+include "../funciones/funciones.php";
 conectarse();
 
-$sql = mysql_query("select fechayhora from registro_transacciones where usuario = '".$login."' and estacion = '".$pc."' order by idregistro_transacciones DESC");
-$bus = mysql_fetch_array($sql);
-$hora_completa_registro = explode(" ", $bus["fechayhora"]); 
-$minutos_registro = explode(":", $hora_completa_registro[1]);
+$sql                    = mysql_query("select fechayhora from registro_transacciones where usuario = '" . $login . "' and estacion = '" . $pc . "' order by idregistro_transacciones DESC");
+$bus                    = mysql_fetch_array($sql);
+$hora_completa_registro = explode(" ", $bus["fechayhora"]);
+$minutos_registro       = explode(":", $hora_completa_registro[1]);
 
-$cantidad_minutos_registro = ($minutos_registro[0]*60)+$minutos_registro[1];
-$cantidad_minutos_actual = (date("H")*60)+date("i");
+$cantidad_minutos_registro = ($minutos_registro[0] * 60) + $minutos_registro[1];
+$cantidad_minutos_actual   = (date("H") * 60) + date("i");
 
-$minutos = $cantidad_minutos_actual-$cantidad_minutos_registro;
+$minutos = $cantidad_minutos_actual - $cantidad_minutos_registro;
 
-if($minutos > 30){
-	echo "mayor";
-}else{
-	echo "menor";
+if ($minutos > 30) {
+    echo "mayor";
+} else {
+    echo "menor";
 }
-?>
